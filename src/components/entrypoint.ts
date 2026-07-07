7bac64aa1e98573be5b1f15b4b97cefe6f5f7b96
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-07-06 19:58:24 -0600

    refactor(entrypoint): restructure dashboard sync logic and remove homepage component feat(farm-context-menu): add farm context menu functionality and project linking feat(settings): create settings component

--- src/components/entrypoint.ts
+++ src/components/entrypoint.ts
@@ -1,4 +1,96 @@
-import { syncHomepageDashboard } from "./homepage"
+import { doTopbarstuff } from "./topbar"
+import type { Information } from "./types"
+import { defaultProfilePfp } from "./consts.ts" // TODO: temp
+import { renderProjects, prepareDashboard, resetProjectsRenderCache } from "./projects"
+import { getInfo } from "./info"
+import { setProjectPopupHooks } from "./popups"
+import { installDashboardProjectSync, type DashboardProjectSyncState } from "./dashboard-projects"
+import { createFarmAreaLinker } from "./farm-context-menu"
+
+let dashboardSync: (() => void) | null = null
+
+function syncHomepageDashboard() {
+    setupHomepageDashboard()
+    if (dashboardSync) {
+        dashboardSync()
+    }
+}
+
+function setupHomepageDashboard() {
+    window.macondo = window.macondo || {}
+    if (location.pathname !== "/dashboard") { return }
+    if (dashboardSync) { return }
+    console.log("macondo+: homepagething running")
+
+    let information: Information = {
+        "user": {
+            "name": "not found",
+            "pfp": defaultProfilePfp
+        },
+        "projects": []
+    }
+
+    let didTryGetInfo = false
+    let didLoadProjects = false
+    let projectSyncState: DashboardProjectSyncState = {
+        activeProjectPopupId: null,
+        activeProjectPopupSnapshot: null,
+        pendingDeleteProjectId: null,
+        isWaitingForNewProjectClose: false,
+        didSeeNewProjectPopup: false
+    }
+    let farmAreaLinker = createFarmAreaLinker(information)
+
+    setProjectPopupHooks({
+        isLinkingFarmAreas: farmAreaLinker.isLinkingFarmAreas,
+        resetLinkedFarmAreasKey: farmAreaLinker.resetLinkedFarmAreasKey,
+        linkFarmAreasToProjects: farmAreaLinker.linkFarmAreasToProjects,
+        rightclickandextractfarmtile: farmAreaLinker.rightclickandextractfarmtile,
+        setNewProjectCloseWait: function(isWaiting, didSeePopup) {
+            projectSyncState.isWaitingForNewProjectClose = isWaiting
+            projectSyncState.didSeeNewProjectPopup = didSeePopup
+        },
+        setActiveProjectPopup: function(projectId, snapshot) {
+            projectSyncState.activeProjectPopupId = projectId
+            projectSyncState.activeProjectPopupSnapshot = snapshot
+        }
+    })
+
+    function loadInfo() {
+        getInfo(didTryGetInfo, information, didLoadProjects, function() {
+            didLoadProjects = true
+            resetProjectsRenderCache()
+            renderProjects(information, didLoadProjects)
+            farmAreaLinker.linkFarmAreasToProjects()
+        })
+    }
+
+    let dashboardProjectSync = installDashboardProjectSync({
+        information: information,
+        state: projectSyncState,
+        getDidLoadProjects: function() {
+            return didLoadProjects
+        },
+        resetLinkedFarmAreasKey: farmAreaLinker.resetLinkedFarmAreasKey,
+        linkFarmAreasToProjects: farmAreaLinker.linkFarmAreasToProjects,
+        loadInfo: loadInfo
+    })
+
+    function syncDashboardState() {
+        if (location.pathname !== "/dashboard") { return }
+        prepareDashboard()
+        if (!didTryGetInfo) {
+            didTryGetInfo = true
+            loadInfo()
+        }
+        dashboardProjectSync.syncNewProjectClose()
+        dashboardProjectSync.syncActiveProjectFromPopupDom()
+        renderProjects(information, didLoadProjects)
+        doTopbarstuff(information)
+    }
+
+    dashboardSync = syncDashboardState
+}
 
 function syncDashboard() {
     if (location.pathname !== "/dashboard") { return }
